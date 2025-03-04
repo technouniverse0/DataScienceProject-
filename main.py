bd2c7e2948d7194af13d059a9e66f568fadaec47
[Bhavik Ramina] <lt3488168@gmail.com>
2025-03-04 12:39:00 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
-logger.info("welcome to our custom logging data sicence")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.initiate_data_ingestion()
+        logger.info(f">>>>>>>>> stage {STAGE_NAME} completed<<<<<<<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+        logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+        data_validation = DataValidationTrainingPipeline()
+        data_validation.initiate_data_validation()
+        logger.info(f">>>>>>>>> stage {STAGE_NAME} completed<<<<<<<<<<\n\nx===========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
